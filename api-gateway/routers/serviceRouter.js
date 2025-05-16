a332d944300f79ed800037f5e337153d7766c027
TalIQ501 <talhaiqbal501@gmail.com>
2025-05-16 03:40:33 -0700

    Gateway Routers updated

--- api-gateway/routers/serviceRouter.js
+++ api-gateway/routers/serviceRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { authRouter } from "./authRouter.js";
 import { usersRouter } from "./userRouter.js";
+import { userTypeRouter } from "./userTypeRouter.js";
 
 export const serviceRouter = Router();
 
 serviceRouter.use('/auth', authRouter)
 serviceRouter.use('/users', usersRouter)
+serviceRouter.use('/usertype', userTypeRouter)
 
