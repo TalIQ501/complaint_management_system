88c7ae7d479fcf0c4f34cf426f4f4b88cd21053a
TalIQ501 <talhaiqbal501@gmail.com>
2025-05-16 00:19:11 -0700

    Completed UserType model

--- services/user-service/models/UserType.js
+++ services/user-service/models/UserType.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const UserTypeSchema = new Schema({
-    user_type: String,
-    trs_time: Date,
+    userType: String,
+    trsTime: Date
 })
+
+export const UserType = mongoose.model('UserType', UserTypeSchema)
