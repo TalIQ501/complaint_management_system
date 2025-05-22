fd034ce9d79bc8d04a5a64a5f2077f145614e6fd
TalIQ501 <talhaiqbal501@gmail.com>
2025-05-22 02:27:07 -0700

    Added valididation error for missing fields

--- services/company-service/controllers/registerCompanyController.js
+++ services/company-service/controllers/registerCompanyController.js
@@ -6,6 +6,7 @@ export const registerCompanyController = async (req, res) => {
         await newCompany.save()
         return res.status(201).json({ message: `Company ${newCompany.companyName} registered` })
     } catch (err) {
+        if (err.name === 'ValidationError') return res.status(400).json({ error : 'Fields missing' })
         res.status(500).json({ error: 'Something went wrong' })
     }
 }
